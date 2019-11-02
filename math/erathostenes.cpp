989499e41a75fbb9e6dbe6d3eae9402c8a077bcf
sarashin <sarashina.nikki65@gmail.com>
2019-11-02 13:52:05 +0900

    fix Erathostenes

--- math/erathostenes.cpp
+++ math/erathostenes.cpp
@@ -1,36 +1,91 @@
-#include <iostream>
-#include <utility>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const long long LINF = 1e18;
+#define dump(x)  cout << 'x' << ' = ' << (x) << ` `;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define REPR(i,n) for(int i=n;i>=0;i--)
+#define FOREACH(x,a) for(auto& (x) : (a) )
 typedef long long ll;
 using namespace std;
+typedef pair<ll, ll> P;
 
-vector<int> primes;
-
-void erathosthenes(int n) {
-    vector<bool> primeFlags(n+10, true);
-    primeFlags[0] = primeFlags[1] = false;
-    vector<int> sqrtprimes;
-    for (int i = 2; i*i <= n; i++) {
-        bool mod0 = false;
-        for (int j = 2; j*j <= i; j++) {
-            if (i%j == 0) mod0 = true;
-        }
-        if (!mod0) sqrtprimes.push_back(i);
+template<typename T>
+void print(const vector<T> &x) {
+    int n = x.size();
+    rep(i,n) {
+        cout << x[i];
+        if (i!=n-1) cout<<" ";
+        else cout << endl;
     }
-    for (int i = 0; i < sqrtprimes.size(); i++) {
-        for (int j = 2; j <= n; j++) {
-            if (j%sqrtprimes[i] == 0) primeFlags[j] = false;
-        }
-    }
-    for (auto &&e: sqrtprimes) primeFlags[e] = true;
-    for (int i = 1; i <= n; i++) {
-        if (primeFlags[i]) primes.push_back(i);
-    }
-    return;
 }
 
-int main() {
-    int n; cin >> n;
-    erathosthenes(n);
+template<typename T>
+void print(const vector<vector<T>> &x) {
+    int n = x.size();
+    rep(i,n) {
+        rep(j,x[i].size()) {
+            cout << x[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T>
+void print(const vector<T> &x, int n) {
+    rep(i,n) {
+        cout << x[i];
+        if (i!=n-1) cout<<" ";
+        else cout << endl;
+    }
+}
+
+template<typename T>
+void print(const vector<vector<T>> &x, int n, int m) {
+    rep(i,n) {
+        rep(j,m) {
+            cout << x[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+void input_init() {
+    cin.tie(0); ios::sync_with_stdio(false);
+}
+
+class Erathostenes {
+private:
+    vector<int> primes;
+public:
+    Erathostenes(int n);
+    vector<int> get_prime();
+    ~Erathostenes();
+};
+
+Erathostenes::Erathostenes(int n) {
+    vector<bool> prime_flags(n+10, true);
+    prime_flags[0] = prime_flags[1] = false;
+    for (int i = 2; i*i <= n; ++i) {
+        if (prime_flags[i]) {
+            primes.push_back(i);
+            for (int j = i; j <= n; j+=i) {
+                prime_flags[j] = false;
+            }
+        }
+    }
+    for (int i = 0; i <= n; ++i) {
+        if (prime_flags[i]) primes.push_back(i);
+    }
+}
+
+vector<int> Erathostenes::get_prime() {
+    return primes;
+}
+
+Erathostenes::~Erathostenes() {
 }
