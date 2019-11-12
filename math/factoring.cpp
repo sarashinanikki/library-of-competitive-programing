9db5d694f25da09ddcb6192fbc223a30df6e9d9a
sarashin <sarashina.nikki65@gmail.com>
2019-11-12 23:28:04 +0900

    modify

--- math/factoring.cpp
+++ math/factoring.cpp
@@ -1,52 +1,100 @@
 // --------------以下ライブラリ---------------------------------//
 
-#include <iostream>
-#include <utility>
-#include <map>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <functional>
-#include <array>
-#include <math.h>
-#include <numeric>
-#include <sstream>
+#include <bits/stdc++.h>
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const long long LINF = 1e18;
+#define dump(x)  cout << 'x' << ' = ' << (x) << ` `;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define REPR(i,n) for(int i=n;i>=0;i--)
+#define FOREACH(x,a) for(auto& (x) : (a) )
 typedef long long ll;
 using namespace std;
+typedef pair<ll, ll> P;
 
+template<typename T>
+void print(const vector<T> &x) {
+    int n = x.size();
+    rep(i,n) {
+        cout << x[i];
+        if (i!=n-1) cout<<" ";
+        else cout << endl;
+    }
+}
 
-class Factoring {
-private:
-    vector<int> primes;
-    map<int, int> factors;
+template<typename T>
+void print(const vector<vector<T>> &x) {
+    int n = x.size();
+    rep(i,n) {
+        rep(j,x[i].size()) {
+            cout << x[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T>
+void print(const vector<T> &x, int n) {
+    rep(i,n) {
+        cout << x[i];
+        if (i!=n-1) cout<<" ";
+        else cout << endl;
+    }
+}
+
+template<typename T>
+void print(const vector<vector<T>> &x, int n, int m) {
+    rep(i,n) {
+        rep(j,m) {
+            cout << x[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+void input_init() {
+    cin.tie(0); ios::sync_with_stdio(false);
+}
+
+class Erathostenes {
 public:
-    Factoring();
+    vector<ll> primes;
+    Erathostenes(ll n);
+    ~Erathostenes();
+};
+
+Erathostenes::Erathostenes(ll n) {
+    vector<bool> prime_flags(n+10, true);
+    prime_flags[0] = prime_flags[1] = false;
+    for (int i = 2; i*i <= n; ++i) {
+        if (prime_flags[i]) {
+            primes.push_back(i);
+            for (int j = i; j <= n; j+=i) {
+                prime_flags[j] = false;
+            }
+        }
+    }
+    for (int i = 0; i <= n; ++i) {
+        if (prime_flags[i]) primes.push_back(i);
+    }
+}
+
+Erathostenes::~Erathostenes() {}
+
+
+class Factoring: public Erathostenes {
+public:
+    map<ll, int> factors;
+    Factoring(ll n):Erathostenes(n) {}
     ~Factoring();
     void factoring(ll n);
 };
 
-Factoring::Factoring() {
-    ll n = sqrt(1e12);
-    vector<bool> primeFlags(n+10, true);
-    primeFlags[0] = primeFlags[1] = false;
-    vector<int> sqrtprimes;
-    for (int i = 2; i*i <= n; i++) {
-        bool mod0 = false;
-        for (int j = 2; j*j <= i; j++) {
-            if (i%j == 0) mod0 = true;
-        }
-        if (!mod0) sqrtprimes.push_back(i);
-    }
-    for (auto &&e: sqrtprimes) {
-        for (int i = 2; i <= n; i++) {
-            if (i%e == 0) primeFlags[i] = false;
-        }
-    }
-    for (auto &&e: sqrtprimes) primeFlags[e] = true;
-    for (int i = 1; i <= n; i++) if (primeFlags[i]) primes.push_back(i);
-    return;
-}
- 
+
 void Factoring::factoring(ll n) {
     for (auto &&e: primes) {
         if (e > n) break;
